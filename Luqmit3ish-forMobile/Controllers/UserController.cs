c3ae5350c6616745f2612e186c19ea0d718a7bd0
Ibtihaj7 <ibtihaj.adham7@gmail.com>
2023-03-27 15:52:09 +0300

    Create Get user by email, Post user and Put user by id APIs

--- Luqmit3ish-forMobile/Controllers/UserController.cs
+++ Luqmit3ish-forMobile/Controllers/UserController.cs
@@ -42,8 +42,81 @@ namespace Luqmit3ish_forMobile.Controllers
             }
             
         }
+        
+        [HttpGet("{email}")]
+        public async Task<ActionResult<User>> GetUserByEmail(string email)
+        {
+            try
+            {
+                if (_context is null)
+                {
+                    return NotFound();
+                }
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var user = await _context.User.FirstOrDefaultAsync(u => u.email == email);
+                if (user is null)
+                {
+                    return NotFound();
+                }
+                return user;
+            }
+
+            catch (Exception e)
+            {
+                return StatusCode(500, "Internal server error " + e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser([FromBody]User user)
+        {
+            try
+            {
+                if (_context is null)
+                {
+                    return NotFound();
+                }
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                _context.User.Add(user);
+                await _context.SaveChangesAsync();
+                
+                return CreatedAtAction("GetUsers", new { id = user.id }, user);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "Internal server error" + e.Message);
+            }
+        }
 
 
-
+        [HttpPut("{id}")]
+        public async Task<ActionResult<User>> UpdateUser(int id, [FromBody]User user)
+        {
+            try
+            {
+                if (_context is null)
+                {
+                    return NotFound();
+                }
+                if (id != user.id || !ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                _context.Entry(user).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "Internal server error" + e.Message);
+            }
+        }
     }
 }

f9835fdac1ff64d923669cdcfdcac53d526e1214
ayaabusoud <ayaabusoud23@gmail.com>
2023-04-19 00:57:32 +0300

    LUQMIT3ISH-86-View-restaurant-of-the-month

---
+++ Luqmit3ish-forMobile/Models/DishesOrder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Luqmit3ish_forMobile.Models
+{
+    public class DishesOrder
+    {
+        public string RestaurantName { get; set; }
+        public int Dishes { get; set; }
+    }
+}
